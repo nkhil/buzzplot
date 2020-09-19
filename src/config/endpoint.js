89dba5e9fbd0a97ad50b1051cbc778342629411a
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2020-09-19 16:08:38 +0100

    ✨implement basic api call and render

---
+++ src/config/endpoint.js
@@ -0,0 +1,7 @@
+
+const HN_URI = 'https://hacker-news.firebaseio.com/v0'
+
+module.exports = {
+  NEW_STORIES: `${HN_URI}/newstories.json`,
+  ITEM: `${HN_URI}/item`,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import axios from 'axios'
+import { NEW_STORIES, ITEM } from '../config/endpoint'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [stories, setStories] = useState([])
+
+  async function getStories() {
+    try {
+      const { data: storyIds } = await axios.get(NEW_STORIES)
+      const promises = storyIds.slice(0, 10).map(id => {
+        return axios.get(`${ITEM}/${id}.json`)
+      })
+      const items = await Promise.all(promises)
+      setStories(items)
+    } catch (err) {
+      console.log('An Error occured in the Home component')
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getStories()
+  }, [])
+
+  return (
+    <>
+      <h1>Stories</h1>
+      {stories.map(story => (
+        <li key={story.data.id}>
+          <a href={story.data.url}>
+            {story.data.title}
+          </a>
+        </li>
+      ))}
+    </>
+  )
 }
